8b51c046c10ac25a607e6f3e1c5d24dc2ad7e5c6
wellflat <wellflat@gmail.com>
2024-11-13 19:01:49 +0900

    changed environment rye to uv

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@
 import os
 from dotenv import load_dotenv
 import chainlit as cl
+from chainlit.input_widget import Slider
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
@@ -12,10 +13,44 @@ from typing import cast
 
 load_dotenv()
 
+@cl.set_chat_profiles
+async def chat_profile():
+    return [
+        cl.ChatProfile(
+            name="GPT-3.5",
+            markdown_description="The underlying LLM model is **GPT-3.5**.",
+            icon="https://picsum.photos/200",
+        ),
+        cl.ChatProfile(
+            name="GPT-4",
+            markdown_description="The underlying LLM model is **GPT-4**.",
+            icon="https://picsum.photos/250",
+        ),
+    ]
+
 @cl.on_chat_start
 async def on_chat_start():
-    api_key = os.getenv("OPENAI_APIKEY") 
-    model = ChatOpenAI(model="gpt-4o", streaming=True, api_key=api_key)
+    settings = await cl.ChatSettings(
+        [
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+        ]
+    ).send()
+    chat_profile = cl.user_session.get("chat_profile")
+    print(chat_profile)
+    api_key = os.getenv("OPENAI_API_KEY")
+    model = ChatOpenAI(
+        model="gpt-4o",
+        streaming=True,
+        temperature=settings["Temperature"],
+        api_key=api_key
+    )
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", "You're a helpful assistant."),
@@ -28,6 +63,8 @@ async def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
+    print(cl.chat_context.to_openai())
+    
     runnable = cast(Runnable, cl.user_session.get("runnable"))
     res = cl.Message(content="")
     async for chunk in runnable.astream(
@@ -36,9 +73,7 @@ async def on_message(message: cl.Message):
     ):
         await res.stream_token(chunk)
     
-    await res.send()
-
-    
+    await res.update()
 
 if __name__ == '__main__':
-    print('Hello, World!')
+    print('Hello, Chainlit')
